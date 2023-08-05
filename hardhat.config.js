80b500cf84f106f03500072860989f6ed42b27b1
Mercien <33868353+Mercien@users.noreply.github.com>
2023-08-05 17:23:23 +0100

    push

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+// import ('@nomicfoundation/hardhat-toolbox');
+
+// // const privateKey = "Wallet Private key"; // my wallet private key polygon mumbai testnework
+// //  alchemy polygone mumbai testnet api key
+// // const INFURA_API_KEY = "9cb128774f4b4f5797fc40a6ff71eded"; //Infura apikey
+
+// module.exports = {
+//   solidity: '0.8.10',
+//   networks: {
+//     hardhat: {},
+//     mumbai: {
+//       url: `https://polygon-mumbai.infura.io/v3/${INFURA_API_KEY}`,
+//       accounts: [],
+//     },
+//   },
+// };
---
+++ pages/_app.js
@@ -0,0 +1,51 @@
+import Head from 'next/head';
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+import NextNProgress from 'nextjs-progressbar';
+import { NFTProvider } from '../context/NFTcontext';
+import '../styles/globals.css';
+import { Navbar, Footer } from '../components';
+
+const App = ({ Component, pageProps }) => (
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div
+        style={{
+          backgroundImage: "url('/blurBackground.png')",
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+        }}
+        className="dark:bg-nft-dark"
+      >
+        <Head>
+          <title>pokemonmarket.com</title>
+          <meta
+            name="description"
+            content="Market NFT where you can buy and sell new NFTs."
+          />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="icon" href="/favicon.ico" />
+          <Script async src="https://www.thatquery.com/script.js" data-website-id="ea038c3c-b76f-4af4-80b8-a16f676e0f44" />
+        </Head>
+        <NextNProgress
+          color="#EB1484"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow
+          options={{ showSpinner: false }}
+        />
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </div>
+
+      <Script src="https://kit.fontawesome.com/d45b25ceeb.js" crossorigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
+);
+
+export default App;
